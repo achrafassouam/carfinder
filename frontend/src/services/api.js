999133eddf0b208684b849c8c8ba6d194dc1264a
Achraf Assouam <achrafassouam@gmail.com>
2024-10-22 22:50:48 +0100

    app.api fix v.2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL ;
+const API_URL = process.env.REACT_APP_BACKEND_API;
 
 export const fetchOptions = async () => {
   try {
